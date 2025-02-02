260b9c2feb081321f76fec3e78f8b47ccd69dfee
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-02 17:06:21 +0200

    Added color constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
+
 class RenitSize {
   static late double width;
   static late double height;
 }
 
-class RenitColor {}
+class RenitColor {
+  static late Color background;
+  static late Color white;
+  static late Color yellow;
+}
