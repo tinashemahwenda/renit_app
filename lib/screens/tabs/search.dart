711382287963600fa1ad988ce8c2bca8e55371a7
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-03 21:23:21 +0200

    Created a saerch page view

--- lib/screens/tabs/search.dart
+++ lib/screens/tabs/search.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-class Search extends StatelessWidget {
-  const Search({super.key});
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [
-          Text(
-            'Search',
-            style: ShadTheme.of(context).textTheme.h2,
-          )
-        ],
-      )),
-    );
+    return SafeArea(
+        child: Column(
+      children: [
+        Text(
+          'Search',
+          style: ShadTheme.of(context).textTheme.h2,
+        )
+      ],
+    ));
   }
 }
