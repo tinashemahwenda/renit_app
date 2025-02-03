dcd2e2c03d85757ef2bcb67a86f4f027874215f1
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-03 21:25:12 +0200

    Created a view for saved jobs

---
+++ lib/screens/tabs/saved.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class SavedJobsPage extends StatelessWidget {
+  const SavedJobsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+      children: [
+        Text(
+          'My Jobs',
+          style: ShadTheme.of(context).textTheme.h3,
+        ),
+      ],
+    ));
+  }
+}
