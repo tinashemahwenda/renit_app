0f03b1467a8fc9e88e49486869d20c957c5e625f
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-04 08:13:11 +0200

    Removed unnecesary import

--- lib/screens/tabs/saved.dart
+++ lib/screens/tabs/saved.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:renit_app/constants/constants.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
