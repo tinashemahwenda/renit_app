ef84a66789af7318387b334c7cb77377e12c8042
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-04 08:45:52 +0200

    Created a simple view for account settings

--- lib/screens/tabs/settings.dart
+++ lib/screens/tabs/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:renit_app/constants/constants.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -9,10 +11,25 @@ class SettingsPage extends StatelessWidget {
     return SafeArea(
         child: Column(
       children: [
-        Text(
-          'Settings',
-          style: ShadTheme.of(context).textTheme.h3,
+        Center(
+          child: Text(
+            'My Settings',
+            style: ShadTheme.of(context).textTheme.h3,
+          ),
         ),
+        SizedBox(height: 40),
+        ShadAlert(
+          icon: Icon(FontAwesomeIcons.circleExclamation),
+          title: Text(
+            'These are your account setting. You can set your personal preferences in here',
+            style: ShadTheme.of(context).textTheme.small,
+          ),
+          decoration: ShadDecoration(
+            color: RenitColor.yellow.withAlpha(80),
+          ),
+        ),
+        SizedBox(height: 100),
+        Image.asset('assets/images/account-settings.png')
       ],
     ));
   }
