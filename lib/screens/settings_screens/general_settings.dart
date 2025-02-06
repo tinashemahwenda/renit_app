79b784c7c2019ca957a226193dea444c36536f08
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-06 15:31:19 +0200

    Updated the navbar for general settings

--- lib/screens/settings_screens/general_settings.dart
+++ lib/screens/settings_screens/general_settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
+import 'package:renit_app/widgets/renit_internal_navbar.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class GeneralSettings extends StatelessWidget {
@@ -17,19 +18,7 @@ class GeneralSettings extends StatelessWidget {
           ),
           child: Column(
             children: [
-              Row(
-                children: [
-                  InkWell(
-                      onTap: () => Navigator.pop(context),
-                      child: Icon(Icons.arrow_back_ios_new)),
-                  Spacer(),
-                  Text(
-                    'General Settings',
-                    style: ShadTheme.of(context).textTheme.h4,
-                  ),
-                  Spacer(),
-                ],
-              ),
+              RenitInternalNavbar(navbarTitle: 'General Settings'),
               SizedBox(
                 height: 40,
               ),
