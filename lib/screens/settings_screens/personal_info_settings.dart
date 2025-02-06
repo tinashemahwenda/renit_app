29c8dc85fe9cf2ba17553fb8a6cb785f842a018e
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-06 15:27:58 +0200

    Added a navbar for personal information

--- lib/screens/settings_screens/personal_info_settings.dart
+++ lib/screens/settings_screens/personal_info_settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class PersonalInfoSettings extends StatelessWidget {
   const PersonalInfoSettings({super.key});
@@ -8,8 +9,27 @@ class PersonalInfoSettings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RenitColor.background,
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  InkWell(
+                      onTap: () => Navigator.pop(context),
+                      child: Icon(Icons.arrow_back_ios_new)),
+                  Spacer(),
+                  Text(
+                    'Personal Information',
+                    style: ShadTheme.of(context).textTheme.h4,
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
