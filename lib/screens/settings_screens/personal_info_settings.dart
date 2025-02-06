f336c0ae19c97f0775933fc6e0637a7fe470a212
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-06 15:30:16 +0200

    Updated the navbar in personal settings

--- lib/screens/settings_screens/personal_info_settings.dart
+++ lib/screens/settings_screens/personal_info_settings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:renit_app/widgets/renit_internal_navbar.dart';
 
 class PersonalInfoSettings extends StatelessWidget {
   const PersonalInfoSettings({super.key});
@@ -13,21 +13,7 @@ class PersonalInfoSettings extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
           child: Column(
-            children: [
-              Row(
-                children: [
-                  InkWell(
-                      onTap: () => Navigator.pop(context),
-                      child: Icon(Icons.arrow_back_ios_new)),
-                  Spacer(),
-                  Text(
-                    'Personal Information',
-                    style: ShadTheme.of(context).textTheme.h4,
-                  ),
-                  Spacer(),
-                ],
-              ),
-            ],
+            children: [RenitInternalNavbar(navbarTitle: 'Personal Settings')],
           ),
         ),
       ),
