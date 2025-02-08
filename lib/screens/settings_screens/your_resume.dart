137fd6112fa5fae4709203aa8b8a01271d2823c4
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-09 01:33:57 +0200

    Set background color to grey

--- lib/screens/settings_screens/your_resume.dart
+++ lib/screens/settings_screens/your_resume.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:renit_app/constants/constants.dart';
 import 'package:renit_app/widgets/renit_internal_navbar.dart';
 
 class YourResume extends StatelessWidget {
@@ -7,11 +8,20 @@ class YourResume extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: RenitColor.background,
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.all(20),
         child: Column(
-          children: [RenitInternalNavbar(navbarTitle: 'Your Resume')],
+          children: [
+            RenitInternalNavbar(navbarTitle: 'Your Resume'),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: RenitSize.width,
+            )
+          ],
         ),
       )),
     );
