ef0a2d5bde5abae77a7914c34406038e6291da7b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-09 00:02:06 +0200

    Finished styling resume creation page

--- lib/screens/settings_screens/resume_page.dart
+++ lib/screens/settings_screens/resume_page.dart
@@ -10,37 +10,137 @@ class ResumePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RenitColor.background,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 20,
-          ),
-          child: Column(
-            children: [
-              RenitInternalNavbar(navbarTitle: 'Your Resume'),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: RenitSize.width,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 10,
-                  children: [
-                    Text(
-                      'Your Resume',
-                      style: ShadTheme.of(context).textTheme.table,
-                    ),
-                    Text('Personal Summary'),
-                  ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: Column(
+              children: [
+                RenitInternalNavbar(navbarTitle: 'Your Resume'),
+                SizedBox(
+                  height: 20,
                 ),
-              )
-            ],
+                Container(
+                  width: RenitSize.width,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 20,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Your Resume',
+                            style: ShadTheme.of(context).textTheme.table,
+                          ),
+                          ShadButton.outline(
+                            child: Text('Upload Resume'),
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'Personal Summary',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                              'Add a personal summary to your profile as a way to introduce yourself'),
+                          ShadButton.outline(
+                            child: Text('Add summary'),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'Education',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                              'Tell your employee about your education history'),
+                          ShadButton.outline(
+                            child: Text('Add education'),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'Skills',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('Tell us about our skills'),
+                          ShadButton.outline(
+                            child: Text('Add skill'),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'License & Certificates',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                              'List down the certificates & licenses you have'),
+                          ShadButton.outline(
+                            child: Text('Add license or certificates'),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 10,
+                        children: [
+                          Text(
+                            'Work Experience',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('Tell us about your previous experience'),
+                          ShadButton.outline(
+                            child: Text('Add experience'),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Spacer(),
+                          ShadButton(
+                            child: Text('Create Resume'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
