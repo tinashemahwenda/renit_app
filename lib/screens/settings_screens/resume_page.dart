40b5ea65dda3289003932d6aee60f308c9715405
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 23:23:01 +0200

    Created a simple view for resume

--- lib/screens/settings_screens/resume_page.dart
+++ lib/screens/settings_screens/resume_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:renit_app/widgets/renit_internal_navbar.dart';
 
 class ResumePage extends StatelessWidget {
   const ResumePage({super.key});
@@ -7,11 +8,16 @@ class ResumePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-              padding: EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 20,
-      ))),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          child: Column(
+            children: [RenitInternalNavbar(navbarTitle: 'Your Resume')],
+          ),
+        ),
+      ),
     );
   }
 }
