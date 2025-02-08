8fb2b0f2c778a9e26260d26071641d22f07a1ae6
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-09 00:18:15 +0200

    Created a simple view for the resume assessment page

--- lib/screens/settings_screens/resume_assessment.dart
+++ lib/screens/settings_screens/resume_assessment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
 import 'package:renit_app/widgets/renit_internal_navbar.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class ResumeAssessment extends StatelessWidget {
   const ResumeAssessment({super.key});
@@ -15,6 +16,31 @@ class ResumeAssessment extends StatelessWidget {
         child: Column(
           children: [
             RenitInternalNavbar(navbarTitle: 'Resume Assessment'),
+            SizedBox(
+              height: 20,
+            ),
+            ShadAlert(
+              icon: Icon(
+                Icons.info,
+                color: Colors.black,
+              ),
+              decoration: ShadDecoration(
+                labelPadding: EdgeInsets.all(10),
+                color: RenitColor.yellow.withAlpha(50),
+              ),
+              descriptionStyle: TextStyle(
+                color: Colors.black,
+              ),
+              description: Text(
+                  'This feature allows you to assess your resume. Upload your resume and get some insights on hirability, quality and more'),
+            ),
+            Container(
+              width: RenitSize.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            )
           ],
         ),
       )),
