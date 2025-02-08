ca7302f3928c8a91ef2c327c06d7e8b67fe08baa
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 21:58:27 +0200

    Added settings options for sign out and deleting account

--- lib/screens/tabs/settings.dart
+++ lib/screens/tabs/settings.dart
@@ -114,6 +114,60 @@ class SettingsPage extends StatelessWidget {
             ],
           ),
         ),
+        SizedBox(height: 20),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          width: RenitSize.width,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            spacing: 10,
+            children: [
+              Icon(
+                Icons.logout_outlined,
+                color: Colors.grey,
+              ),
+              Text(
+                'Sign Out',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          width: RenitSize.width,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            spacing: 10,
+            children: [
+              Icon(
+                Icons.delete_outline_outlined,
+                color: Colors.redAccent,
+              ),
+              Text(
+                'Delete Account',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     ));
   }
