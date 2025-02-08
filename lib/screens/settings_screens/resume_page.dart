40fda9767de906d788a9869e2f8ec80cf27a99a3
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 23:45:31 +0200

    Styled a box for the resume building

--- lib/screens/settings_screens/resume_page.dart
+++ lib/screens/settings_screens/resume_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
 import 'package:renit_app/widgets/renit_internal_navbar.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class ResumePage extends StatelessWidget {
   const ResumePage({super.key});
@@ -16,7 +17,30 @@ class ResumePage extends StatelessWidget {
             vertical: 20,
           ),
           child: Column(
-            children: [RenitInternalNavbar(navbarTitle: 'Your Resume')],
+            children: [
+              RenitInternalNavbar(navbarTitle: 'Your Resume'),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: RenitSize.width,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 10,
+                  children: [
+                    Text(
+                      'Your Resume',
+                      style: ShadTheme.of(context).textTheme.table,
+                    ),
+                    Text('Personal Summary'),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
