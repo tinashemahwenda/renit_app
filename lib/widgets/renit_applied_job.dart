434da14654a9d829302150e1cf10b8a7299dc75b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-09 02:52:42 +0200

    Added some padding to the jobs card

--- lib/widgets/renit_applied_job.dart
+++ lib/widgets/renit_applied_job.dart
@@ -17,35 +17,38 @@ class RenitAppliedJobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: RenitSize.width,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-      ),
-      child: Column(
-        spacing: 5,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            roleName,
-            style: ShadTheme.of(context).textTheme.table,
-          ),
-          Text(roleCompany),
-          Text('Status: $progressStatus'),
-          SizedBox(height: 10),
-          ShadProgress(
-            value: progressValue,
-            innerBorderRadius: BorderRadius.circular(10),
-            minHeight: 8,
-            color: progressValue >= 0.25
-                ? Colors.red
-                : progressValue >= 0.5
-                    ? Colors.amberAccent
-                    : Colors.green,
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Container(
+        width: RenitSize.width,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
+        child: Column(
+          spacing: 5,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              roleName,
+              style: ShadTheme.of(context).textTheme.table,
+            ),
+            Text(roleCompany),
+            Text('Status: $progressStatus'),
+            SizedBox(height: 10),
+            ShadProgress(
+              value: progressValue,
+              innerBorderRadius: BorderRadius.circular(10),
+              minHeight: 8,
+              color: progressValue >= 0.25
+                  ? Colors.red
+                  : progressValue >= 0.5
+                      ? Colors.amberAccent
+                      : Colors.green,
+            )
+          ],
+        ),
       ),
     );
   }
