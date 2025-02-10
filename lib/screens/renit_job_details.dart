bd048c00657ef694587f28ddbcae6a08a2273c65
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-10 23:45:56 +0200

    Creatd some job description cards

--- lib/screens/renit_job_details.dart
+++ lib/screens/renit_job_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:renit_app/constants/constants.dart';
 //import 'package:renit_app/constants/constants.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
@@ -8,21 +9,24 @@ class RenitJobDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: RenitColor.background,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 40,
-            ),
+          child: SingleChildScrollView(
             child: Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    InkWell(
-                      onTap: () => Navigator.pop(context),
-                      child:
-                          CircleAvatar(child: Icon(Icons.arrow_back_ios_new)),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 40,
+                      ),
+                      child: InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child:
+                            CircleAvatar(child: Icon(Icons.arrow_back_ios_new)),
+                      ),
                     ),
                     Row(
                       spacing: 10,
@@ -32,7 +36,87 @@ class RenitJobDetails extends StatelessWidget {
                       ],
                     )
                   ],
-                )
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: RenitSize.height,
+                  width: RenitSize.width,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(30))),
+                  child: Column(
+                    spacing: 2,
+                    children: [
+                      SizedBox(height: 20),
+                      Text(
+                        'Graphic Designer',
+                        style: ShadTheme.of(context).textTheme.h4,
+                      ),
+                      Text(
+                        'Renit Group',
+                        style: ShadTheme.of(context).textTheme.muted,
+                      ),
+                      Center(
+                        child: Row(
+                          children: [
+                            Spacer(),
+                            Icon(Icons.location_pin),
+                            Text(
+                              'Harare, Zimbabwe',
+                              style: ShadTheme.of(context).textTheme.muted,
+                            ),
+                            Spacer()
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ShadCard(
+                            padding: EdgeInsets.all(10),
+                            width: RenitSize.width / 2,
+                            child: Row(
+                              spacing: 5,
+                              children: [
+                                CircleAvatar(
+                                  child: Icon(Icons.currency_exchange_outlined),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Salary(Monthly)'),
+                                    Text('\$300-\$400'),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          ShadCard(
+                            padding: EdgeInsets.all(10),
+                            width: RenitSize.width / 2,
+                            child: Row(
+                              spacing: 5,
+                              children: [
+                                CircleAvatar(
+                                  child: Icon(Icons.currency_exchange_outlined),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Salary(Monthly)'),
+                                    Text('\$300-\$400'),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
