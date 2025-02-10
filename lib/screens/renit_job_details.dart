2114f8ec298090cb0b1b45a22a5203372793d188
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-10 23:58:55 +0200

    Added tiles for job descriptions

--- lib/screens/renit_job_details.dart
+++ lib/screens/renit_job_details.dart
@@ -47,74 +47,174 @@ class RenitJobDetails extends StatelessWidget {
                       color: Colors.white,
                       borderRadius:
                           BorderRadius.vertical(top: Radius.circular(30))),
-                  child: Column(
-                    spacing: 2,
-                    children: [
-                      SizedBox(height: 20),
-                      Text(
-                        'Graphic Designer',
-                        style: ShadTheme.of(context).textTheme.h4,
-                      ),
-                      Text(
-                        'Renit Group',
-                        style: ShadTheme.of(context).textTheme.muted,
-                      ),
-                      Center(
-                        child: Row(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      spacing: 2,
+                      children: [
+                        SizedBox(height: 20),
+                        Text(
+                          'Graphic Designer',
+                          style: ShadTheme.of(context).textTheme.h4,
+                        ),
+                        Text(
+                          'Renit Group',
+                          style: ShadTheme.of(context).textTheme.muted,
+                        ),
+                        Center(
+                          child: Row(
+                            children: [
+                              Spacer(),
+                              Icon(Icons.location_pin),
+                              Text(
+                                'Harare, Zimbabwe',
+                                style: ShadTheme.of(context).textTheme.muted,
+                              ),
+                              Spacer()
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Spacer(),
-                            Icon(Icons.location_pin),
-                            Text(
-                              'Harare, Zimbabwe',
-                              style: ShadTheme.of(context).textTheme.muted,
+                            ShadCard(
+                              shadows: [BoxShadow(offset: Offset.zero)],
+                              padding: EdgeInsets.all(10),
+                              width: RenitSize.width / 2.3,
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  CircleAvatar(
+                                    child:
+                                        Icon(Icons.currency_exchange_outlined),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Salary',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        '\$300-\$400',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                            Spacer()
+                            ShadCard(
+                              padding: EdgeInsets.all(10),
+                              width: RenitSize.width / 2.3,
+                              shadows: [BoxShadow(offset: Offset.zero)],
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  CircleAvatar(
+                                    child: Icon(Icons.cases_outlined),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Job type',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Full-Time',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                      Row(
-                        children: [
-                          ShadCard(
-                            padding: EdgeInsets.all(10),
-                            width: RenitSize.width / 2,
-                            child: Row(
-                              spacing: 5,
-                              children: [
-                                CircleAvatar(
-                                  child: Icon(Icons.currency_exchange_outlined),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text('Salary(Monthly)'),
-                                    Text('\$300-\$400'),
-                                  ],
-                                )
-                              ],
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ShadCard(
+                              padding: EdgeInsets.all(10),
+                              width: RenitSize.width / 2.3,
+                              shadows: [BoxShadow(offset: Offset.zero)],
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  CircleAvatar(
+                                    child: Icon(Icons.bar_chart_rounded),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Level',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Entry-Level',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                          ShadCard(
-                            padding: EdgeInsets.all(10),
-                            width: RenitSize.width / 2,
-                            child: Row(
-                              spacing: 5,
-                              children: [
-                                CircleAvatar(
-                                  child: Icon(Icons.currency_exchange_outlined),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text('Salary(Monthly)'),
-                                    Text('\$300-\$400'),
-                                  ],
-                                )
-                              ],
+                            ShadCard(
+                              padding: EdgeInsets.all(10),
+                              width: RenitSize.width / 2.3,
+                              shadows: [BoxShadow(offset: Offset.zero)],
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  CircleAvatar(
+                                    child: Icon(Icons.location_city_rounded),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Condition',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Office Work',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
