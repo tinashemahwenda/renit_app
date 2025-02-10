4dd18b07416fe3bff3d76ddae6792e639f53470e
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-10 23:09:27 +0200

    Created a topbar for the job description

--- lib/screens/renit_job_details.dart
+++ lib/screens/renit_job_details.dart
@@ -6,9 +6,30 @@ class RenitJobDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Job Details'),
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 40,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(child: Icon(Icons.arrow_back_ios_new)),
+                Row(
+                  spacing: 10,
+                  children: [
+                    CircleAvatar(child: Icon(Icons.bookmark_add)),
+                    CircleAvatar(child: Icon(Icons.share)),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
