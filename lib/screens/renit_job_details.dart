8df2bd82e6ace2351815254911e184d8eab1e4b4
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-10 23:22:17 +0200

    Created a button for applying

--- lib/screens/renit_job_details.dart
+++ lib/screens/renit_job_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//import 'package:renit_app/constants/constants.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class RenitJobDetails extends StatelessWidget {
   const RenitJobDetails({super.key});
@@ -7,32 +9,45 @@ class RenitJobDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 40,
-        ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
               children: [
-                InkWell(
-                  onTap: () => Navigator.pop(context),
-                  child: CircleAvatar(child: Icon(Icons.arrow_back_ios_new)),
-                ),
                 Row(
-                  spacing: 10,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CircleAvatar(child: Icon(Icons.bookmark_add)),
-                    CircleAvatar(child: Icon(Icons.share)),
+                    InkWell(
+                      onTap: () => Navigator.pop(context),
+                      child:
+                          CircleAvatar(child: Icon(Icons.arrow_back_ios_new)),
+                    ),
+                    Row(
+                      spacing: 10,
+                      children: [
+                        CircleAvatar(child: Icon(Icons.bookmark_add)),
+                        CircleAvatar(child: Icon(Icons.share)),
+                      ],
+                    )
                   ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
-    ));
+        bottomNavigationBar: ShadButton(
+          child: Row(
+            spacing: 5,
+            children: [
+              Icon(
+                Icons.mail_outline_outlined,
+                color: Colors.white,
+              ),
+              Text('Apply for this Job'),
+            ],
+          ),
+        ));
   }
 }
