9da4e554cd1b0ac9a276922a5b172a00f009f680
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-10 22:53:58 +0200

    Created a page for job description view

---
+++ lib/screens/renit_job_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RenitJobDetails extends StatelessWidget {
+  const RenitJobDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Job Details'),
+      ),
+    );
+  }
+}
