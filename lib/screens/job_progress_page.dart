4ab89da5d0b580e8ef2d7e65ce3ec25756c5dfb1
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-12 20:46:27 +0200

    Added a navbar for the tracking job screen

--- lib/screens/job_progress_page.dart
+++ lib/screens/job_progress_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
+import 'package:renit_app/widgets/renit_internal_navbar.dart';
 
 class JobProgressPage extends StatelessWidget {
   const JobProgressPage({super.key});
@@ -8,6 +9,13 @@ class JobProgressPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RenitColor.background,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          children: [RenitInternalNavbar(navbarTitle: 'Tracking Job Progress')],
+        ),
+      )),
     );
   }
 }
