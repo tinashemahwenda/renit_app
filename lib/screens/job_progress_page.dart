d90fb5f6d7f0bdf76c417ba3dbc406299c5763e5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-12 20:55:26 +0200

    Implmented easy stepper package widget

--- lib/screens/job_progress_page.dart
+++ lib/screens/job_progress_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_stepper/easy_stepper.dart';
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
 import 'package:renit_app/widgets/renit_internal_navbar.dart';
@@ -16,14 +17,13 @@ class JobProgressPage extends StatelessWidget {
           children: [
             RenitInternalNavbar(navbarTitle: 'Tracking Job Progress'),
             SizedBox(height: 20),
-            Stepper(steps: [
-              Step(
-                title: Text('Step 1'),
-                content: Text('First Step'),
-                isActive: false,
-                label: Icon(Icons.home),
-              )
-            ])
+            EasyStepper(
+              activeStep: 0,
+              direction: Axis.vertical,
+              steps: [
+                EasyStep(title: 'First Step'),
+              ],
+            )
           ],
         ),
       )),
