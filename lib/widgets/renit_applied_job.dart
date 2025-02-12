73ba145ece808425bb7cd468d4247e4a98a443ff
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-12 22:41:43 +0200

    Added routing to each card

--- lib/widgets/renit_applied_job.dart
+++ lib/widgets/renit_applied_job.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:renit_app/constants/constants.dart';
+import 'package:renit_app/screens/job_progress_page.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class RenitAppliedJobCard extends StatelessWidget {
@@ -17,53 +18,57 @@ class RenitAppliedJobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5),
-      child: Container(
-        width: RenitSize.width,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-        ),
-        child: Column(
-          spacing: 5,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              spacing: 5,
-              children: [
-                Text(
-                  roleName,
-                  style: ShadTheme.of(context).textTheme.table,
-                ),
-                progressValue <= 0.25
-                    ? ShadBadge.outline(child: Text('Pending'))
-                    : progressValue <= 0.5
-                        ? ShadBadge.outline(child: Text('Pending'))
-                        : ShadBadge(
-                            child: Text(
-                              'Success',
+    return InkWell(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => JobProgressPage())),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: Container(
+          width: RenitSize.width,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: Column(
+            spacing: 5,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                spacing: 5,
+                children: [
+                  Text(
+                    roleName,
+                    style: ShadTheme.of(context).textTheme.table,
+                  ),
+                  progressValue <= 0.25
+                      ? ShadBadge.outline(child: Text('Pending'))
+                      : progressValue <= 0.5
+                          ? ShadBadge.outline(child: Text('Pending'))
+                          : ShadBadge(
+                              child: Text(
+                                'Success',
+                              ),
+                              backgroundColor: Colors.green[100],
+                              foregroundColor: Colors.green,
                             ),
-                            backgroundColor: Colors.green[100],
-                            foregroundColor: Colors.green,
-                          ),
-              ],
-            ),
-            Text(roleCompany),
-            Text('Status: $progressStatus'),
-            SizedBox(height: 10),
-            ShadProgress(
-              value: progressValue,
-              innerBorderRadius: BorderRadius.circular(10),
-              minHeight: 8,
-              color: progressValue <= 0.25
-                  ? Colors.red
-                  : progressValue <= 0.5
-                      ? Colors.amberAccent
-                      : Colors.green,
-            )
-          ],
+                ],
+              ),
+              Text(roleCompany),
+              Text('Status: $progressStatus'),
+              SizedBox(height: 10),
+              ShadProgress(
+                value: progressValue,
+                innerBorderRadius: BorderRadius.circular(10),
+                minHeight: 8,
+                color: progressValue <= 0.25
+                    ? Colors.red
+                    : progressValue <= 0.5
+                        ? Colors.amberAccent
+                        : Colors.green,
+              )
+            ],
+          ),
         ),
       ),
     );
