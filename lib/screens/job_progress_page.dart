d79d59d3672cdcbc8590254d1abb63cd8bfea838
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-12 20:44:53 +0200

    Created a view for job progress

---
+++ lib/screens/job_progress_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:renit_app/constants/constants.dart';
+
+class JobProgressPage extends StatelessWidget {
+  const JobProgressPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: RenitColor.background,
+    );
+  }
+}
